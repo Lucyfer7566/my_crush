5d873c177da43ff2bd49e40cc95c6af20bb7f1eb
Lucyfer7566 <95369046+Lucyfer7566@users.noreply.github.com>
2023-08-23 17:36:13 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Love confess",
+    introTitle: 'Này cậu gì ơi 🤗',
+    introDesc: `Khen cậu xinh sẽ thừa mất, đặt cậu vào tim chắc chắn sẽ vừa nhất <3`,
+    btnIntro: '^^HiHi^^',
+    title: 'Làm người yêu tớ nha 🥰',
+    desc: '',
+    btnYes: 'Thích lắm 🥰',
+    btnNo: 'Không nha 😗',
+    question: 'Cho tớ biết lí do đi 😊',
+    btnReply: 'Câu trả lời nè 😘',
+    reply: 'Yêu thì yêu mà không yêu thì yêu ❤',
+    mess: 'Tớ biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
+    messDesc: 'Inbox cho tớ nha ❤.',
+    btnAccept: 'Okiiiii lun ❤',
+    messLink: 'https://www.facebook.com/anhduc4923/'
+}
